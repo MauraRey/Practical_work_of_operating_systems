f1bb7799824c879e4df38638b885a5c6b242d8ad
MauraRey <maxsim3210ghs@gmail.com>
2024-11-06 12:17:38 +0200

    first commit

--- Practical_work_of_romb4/src/main.rs
+++ Practical_work_of_romb4/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let n = 5;
+
+    for i in 0..n {
+        for _ in 0..(n - i - 1) {
+            print!(" ");
+        }
+        for _ in 0..(2 * i + 1) {
+            print!("*");
+        }
+        println!();
+    }
+
+    for i in (0..n - 1).rev() {
+        for _ in 0..(n - i - 1) {
+            print!(" ");
+        }
+        for _ in 0..(2 * i + 1) {
+            print!("*");
+        }
+        println!();
+    }
 }
---
+++ Practical_work_of_romb4/target/debug/deps/Practical_work_of_romb4.d
@@ -0,0 +1,5 @@
+F:\Practical_work_of_operating_systems\Practical_work_of_romb4\target\debug\deps\Practical_work_of_romb4.exe: src/main.rs
+
+F:\Practical_work_of_operating_systems\Practical_work_of_romb4\target\debug\deps\Practical_work_of_romb4.d: src/main.rs
+
+src/main.rs:
